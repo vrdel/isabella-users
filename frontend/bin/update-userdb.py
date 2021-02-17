fe0a209597a32bfc4a1456431fd8efc0f6ae3db4
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-02-17 10:56:14 +0100

    remove not needed functions in cache db frontend update tool

--- frontend/bin/update-userdb.py
+++ frontend/bin/update-userdb.py
@@ -22,56 +22,29 @@ import sys
 conf_opts = parse_config()
 
 
-def diff_users(session, db, passwd):
-    upd = dict()
-
-    users_db = set(db.keys())
-    users_passwd = set(passwd.keys())
-    if len(users_db) != len(users_passwd):
-        users_add = users_passwd.difference(users_db)
-        users_del = users_db.difference(users_passwd)
-        upd = {'add': ' '.join(users_add),
-               'del': ' '.join(users_del)}
-
-    return upd
-
-
 def diff_projects(db, passwd):
     userdiff = dict()
-    dbk = set(db.values())
-    pwdk = set(passwd.values())
 
-    for key, value in passwd.iteritems():
-        if db[key] != passwd[key]:
-            diff = None
-            dbs = set(db[key].split())
-            pws = set(passwd[key].split())
-            if len(dbs) > len(pws):
-                diff = dbs.difference(pws)
-            else:
-                diff = pws.difference(dbs)
-
-            userdiff[key] = {
-                'miss': ' '.join(diff),
-                'all': passwd[key]
+    for username in passwd.keys():
+        rem, add = set([]), set([])
+        if db[username] != passwd[username]:
+            userdiff[username] = {
+                'rem': '',
+                'add': '',
+                'all': ''
             }
+            dbs = set(db[username].split())
+            pws = set(passwd[username].split())
+            if len(dbs) > len(pws):
+                rem = dbs.difference(pws)
+            else:
+                add = pws.difference(dbs)
+
+            userdiff[username]['rem'] = ' '.join(rem)
+            userdiff[username]['add'] = ' '.join(add)
+            userdiff[username]['all'] = passwd[username]
 
     return userdiff
-
-
-def unsign_projectusers(session, pu):
-    if len(pu) >= 1:
-        for k, v in pu.iteritems():
-            users = session.query(User).filter(User.username.in_(v)).all()
-            for u in users:
-                u.last_projects = ''
-
-        session.commit()
-
-        return True
-
-    else:
-        return False
 
 
 def assign_projectusers(session, pu):
@@ -87,20 +60,6 @@ def assign_projectusers(session, pu):
     else:
         return False
 
-
-def db_projects_users(session=None):
-    all_projects_user_db = dict()
-
-    if session:
-        for u in session.query(User).all():
-            p = u.last_projects
-            if p in all_projects_user_db:
-                all_projects_user_db[p].append(u.username)
-            else:
-                all_projects_user_db[p] = list()
-                all_projects_user_db[p].append(u.username)
-
-    return all_projects_user_db
 
 
 def db_users_projects(session=None):
@@ -143,6 +102,7 @@ def main():
     # create new users by comparing user entries in /etc/passwd and in
     # cache.db. if user exists in /etc/passwd but not in cache.db, we have a new
     # one.
+    # TODO: user exist in cache.db but not in /etc/passwd?
     allusers_passwd = set(usertool.all_users_list())
     allusers_db = set([u[0] for u in session.query(User.username).all()])
     diff = allusers_passwd.difference(allusers_db)
@@ -158,6 +118,8 @@ def main():
             groupid = usertool.get_group_id(userobj)
             home = usertool.get_user_home(userobj)
 
+            # create a new entry in cache.db for new user.
+            # initially set his projects and last_projects field the same.
             u = User(username, name, surname, '', shell, home, '', userid,
                      groupid, False, False, False, False, False,
                      datetime.datetime.now(), True, projects, projects)
@@ -165,37 +127,26 @@ def main():
             session.add(u)
 
         session.commit()
-        logger.info("New users added into DB: %s" % str_iterable(diff))
+        logger.info("New users added into DB: %s", str_iterable(diff))
 
     else:
         logger.info("No new users added in /etc/passwd")
 
-    # update (project, user) assignments by creating associations from
-    # /etc/passwd and from cache.db. if they differ, there's new project and
-    # update last_project field for user assigned to new project.
-    all_projects_users_passwd = usertool.all_users_projects()
-    all_projects_users_db = db_users_projects(session)
+    # report differencies in (user, project) assignments by creating and
+    # comparing associations from /etc/passwd and from cache db. set
+    # last_projects field in cache.db to match projects in /etc/passwd.
+    # projects field will have previous value so that create-accounts.py can
+    # notice the difference.
+    all_users_projects_password = usertool.all_users_projects()
+    all_users_projects_db = db_users_projects(session)
 
-    pjs = diff_projects(all_projects_users_db, all_projects_users_passwd)
+    pjs = diff_projects(all_users_projects_db, all_users_projects_password)
     if assign_projectusers(session, pjs):
-        for k, v in pjs.iteritems():
-            logger.info('User %s assigned to project %s' % (k, v['miss']))
-
-    # update user assignments to existing projects changing his last_project
-    # field
-    all_projects_users_db = db_users_projects(session)
-    upjs = diff_users(session, all_projects_users_db, all_projects_users_passwd)
-    if upjs:
-        for k, v in upjs.iteritems():
-            ptmp = dict()
-            ptmp[k] = v['add']
-            if ptmp[k] and assign_projectusers(session, ptmp):
-                logger.info('Users %s reassigned to project %s' % (str_iterable(v['add']), k))
-
-            ptmp = dict()
-            ptmp[k] = v['del']
-            if ptmp[k] and unsign_projectusers(session, ptmp):
-                logger.info('Users %s signoff from project %s' % (str_iterable(v['del']), k))
+        for user, assign in pjs.iteritems():
+            if assign['add']:
+                logger.info('User %s assigned to projects %s', user, assign['add'])
+            if assign['rem']:
+                logger.info('User %s sign off projects %s', user, assign['rem'])
 
 
 if __name__ == '__main__':
