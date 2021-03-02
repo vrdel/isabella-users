0a370a435a47a9a65b21ff2236ba0eb8033bfe42
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-03-02 14:37:08 +0100

    calls sdeluser.sh appropriately

--- frontend/helpers/sgetools/sdeluser.sh
+++ frontend/helpers/sgetools/sdeluser.sh
@@ -7,6 +7,8 @@ fi
 QCONF="$SGE_ROOT/bin/$SGE_ARCH/qconf"
 SGE_ADMIN_LOG="SGEAdmin.log"
 
+echo "INFO: Begin removing user $1 / project $2" >> $SGE_ADMIN_LOG
+
 # remove user from ACL
 echo "INFO: Remove user $1 from ACL $2"
 $QCONF -du $1 $2 >> $SGE_ADMIN_LOG 2>&1
@@ -18,5 +20,3 @@ $QCONF -dstnode /$2/$1 >> $SGE_ADMIN_LOG 2>&1
 echo "INFO: End remove user $1 / project $2" >> $SGE_ADMIN_LOG
 
 exit 0
-
-
